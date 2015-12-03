886f2db31eb3857313ec4c54294b25e0869d4b78
solarcult <LiangJingJing@MatrixIII>
2015-12-04 00:27:47 +0800

    this is it

--- src/X.java
+++ src/X.java
@@ -1,10 +1,8 @@
-package fxsol;
 
-public class HelloWorld {
+public class X {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
 
 	}
 
